3615f39434fc1baf5b6959d6de9d9988ca122086
Siniša Bolsšc <sinisa.bolsec0@gmail.com>
2021-02-18 19:00:09 +0100

    linking between pages

--- ninjalist/pages/index.js
+++ ninjalist/pages/index.js
@@ -1,5 +1,6 @@
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -8,6 +9,9 @@ export default function Home() {
       <h1>Homepage</h1>
       <p>Lorem Ipsum</p>
       <p>Lorem Ipsum</p>
+      <Link href="/ninjas">
+        <a>See Ninja Listing</a>
+      </Link>
       <Footer />
     </div>
   )
