4730a19b160f7c3400d9c2ee1973ef4a1daba030
Siniša Bolsšc <sinisa.bolsec0@gmail.com>
2021-02-18 19:19:22 +0100

    adding styles

--- ninjalist/pages/index.js
+++ ninjalist/pages/index.js
@@ -1,15 +1,16 @@
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import styles from '../styles/Home.module.css'
 import Link from 'next/link';
 
 export default function Home() {
   return (
     <div>
-      <h1>Homepage</h1>
-      <p>Lorem Ipsum</p>
-      <p>Lorem Ipsum</p>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       <Link href="/ninjas">
-        <a>See Ninja Listing</a>
+        <a className={styles.btn}>See Ninja Listing</a>
       </Link>
     </div>
   )
