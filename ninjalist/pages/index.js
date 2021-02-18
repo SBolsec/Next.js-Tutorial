cf11f71f03d1d0f249b2bfc20699b6ebf590b2c1
Siniša Bolsšc <sinisa.bolsec0@gmail.com>
2021-02-18 18:54:37 +0100

    adding other components

--- ninjalist/pages/index.js
+++ ninjalist/pages/index.js
@@ -1,10 +1,14 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Homepage</h1>
+      <p>Lorem Ipsum</p>
+      <p>Lorem Ipsum</p>
+      <Footer />
     </div>
   )
 }
